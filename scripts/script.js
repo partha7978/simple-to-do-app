08a493421012ebc100c2404da53f8c2991c5a9d8
partha7978 <muduliparthasarathi@gmail.com>
2021-07-15 17:38:42 +0530

    adding js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
-let addButton = document.querySelector(".inputAddButton") {
-    
-}
+let addButton = document.querySelector(".inputAddButton");
+let ul  = document.querySelector("ul");
+let textInput = document.querySelector(".input");
+let time = document.querySelector(".inputTime");
+let finish = document.querySelector(".fa-check");
+let notFinish = document.querySelector(".fa-times");
