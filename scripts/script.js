8613754483d34f82a4007f1a3fc961b5605d6a1b
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-09-17 20:45:42 +0530

    format doc

--- scripts/script.js
+++ scripts/script.js
@@ -4,81 +4,81 @@ let textInput = document.querySelector(".input");
 let timeAdd = document.querySelector(".inputTime");
 
 const main = () => {
-     // for add
-     let li = document.createElement("li");
-     let rightDiv = document.createElement("div");
-     let middleDiv = document.createElement("div");
-     let leftDiv = document.createElement("div");
- 
-     let serialNo = document.createElement("span");
-     let time = document.createElement("span");
- 
-     let middleText = document.createElement("span");
- 
-     let rightIcon = document.createElement("i");
-     let wrongIcon = document.createElement("i");
- 
-     rightDiv.classList.add("right");
-     middleDiv.classList.add("middle");
-     leftDiv.classList.add("left");
- 
-     serialNo.classList.add("list-number");
-     time.classList.add("time-list");
- 
-     middleText.classList.add("main-list-content");
- 
-     rightIcon.classList.add("fas", "fa-check");
-     wrongIcon.classList.add("fas", "fa-trash-alt");
- 
-     rightDiv.appendChild(serialNo);
-     rightDiv.appendChild(time);
- 
-     middleDiv.appendChild(middleText);
- 
-     leftDiv.appendChild(rightIcon);
-     leftDiv.appendChild(wrongIcon);
- 
-     li.appendChild(rightDiv);
-     li.appendChild(middleDiv);
-     li.appendChild(leftDiv);
- 
-     ul.appendChild(li);
- 
-     middleText.appendChild(document.createTextNode(textInput.value));
-     textInput.value = "";
+    // for add
+    let li = document.createElement("li");
+    let rightDiv = document.createElement("div");
+    let middleDiv = document.createElement("div");
+    let leftDiv = document.createElement("div");
 
-     //for serial number 
-     let lists = document.getElementsByTagName("li");
-     serialNo.innerText = lists.length;
+    let serialNo = document.createElement("span");
+    let time = document.createElement("span");
 
-     //for time adding
-     let link =
-     document.querySelector(
-       ".inputTime").value;
-     time.innerHTML = link;
+    let middleText = document.createElement("span");
 
-     // for remove
-     wrongIcon.onclick = (e) => {
+    let rightIcon = document.createElement("i");
+    let wrongIcon = document.createElement("i");
+
+    rightDiv.classList.add("right");
+    middleDiv.classList.add("middle");
+    leftDiv.classList.add("left");
+
+    serialNo.classList.add("list-number");
+    time.classList.add("time-list");
+
+    middleText.classList.add("main-list-content");
+
+    rightIcon.classList.add("fas", "fa-check");
+    wrongIcon.classList.add("fas", "fa-trash-alt");
+
+    rightDiv.appendChild(serialNo);
+    rightDiv.appendChild(time);
+
+    middleDiv.appendChild(middleText);
+
+    leftDiv.appendChild(rightIcon);
+    leftDiv.appendChild(wrongIcon);
+
+    li.appendChild(rightDiv);
+    li.appendChild(middleDiv);
+    li.appendChild(leftDiv);
+
+    ul.appendChild(li);
+
+    middleText.appendChild(document.createTextNode(textInput.value));
+    textInput.value = "";
+
+    //for serial number 
+    let lists = document.getElementsByTagName("li");
+    serialNo.innerText = lists.length;
+
+    //for time adding
+    let link =
+        document.querySelector(
+            ".inputTime").value;
+    time.innerHTML = link;
+
+    // for remove
+    wrongIcon.onclick = (e) => {
         e.target.parentNode.parentNode.remove();
 
-            //for serial number update After EVERY DELETION---------------
-            let ul = document.querySelector("ul");
+        //for serial number update After EVERY DELETION---------------
+        let ul = document.querySelector("ul");
 
-            let ulArr = [];
-            for(let i= 3; i <= ul.childNodes.length - 1; i++) {
-                ulArr.push(ul.childNodes[i]);
-                ul.childNodes[i].childNodes[0].childNodes[0].innerText = i-2;
-            }
-           
- 
-     };
+        let ulArr = [];
+        for (let i = 3; i <= ul.childNodes.length - 1; i++) {
+            ulArr.push(ul.childNodes[i]);
+            ul.childNodes[i].childNodes[0].childNodes[0].innerText = i - 2;
+        }
 
-     // for toggle
-     rightIcon.addEventListener("click",  () => {
-         middleText.classList.toggle("toggle");
-         rightIcon.classList.toggle("fa-redo-alt");
-         rightIcon.classList.toggle("fa-check");
-     })
+
+    };
+
+    // for toggle
+    rightIcon.addEventListener("click", () => {
+        middleText.classList.toggle("toggle");
+        rightIcon.classList.toggle("fa-redo-alt");
+        rightIcon.classList.toggle("fa-check");
+    })
 }
 
 const addlistAfterClick = () => {
@@ -91,18 +91,14 @@ const addlistAfterClick = () => {
 }
 
 const addListAfterKeypress = (event) => {
-    if (textInput.value.length > 0 && event.keyCode ===13) {
-        if(timeAdd.value > "0") {
+    if (textInput.value.length > 0 && event.keyCode === 13) {
+        if (timeAdd.value > "0") {
             main();
         }
-        else 
-        {
+        else {
             alert("OOps! You forget to add time or content. Please add it  🤗");
         }
     }
 }
 addButton.addEventListener("click", addlistAfterClick);
 textInput.addEventListener("keypress", addListAfterKeypress);
-
-
-
