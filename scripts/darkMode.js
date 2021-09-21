ce9d768be09a143148ce62b514343025f638b8cc
partha7978 <muduliparthasarathi@gmail.com>
2021-09-21 11:40:13 +0530

    dark mode part animation done

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,5 +1,20 @@
 const btn = document.querySelector(".dark-mode-button");
+const i = document.querySelector(".dark-mode-icon");
+let container = document.querySelector(".container");
 
-btn.addEventListener("click", function(){
-    console.log("dark mode icon working");
+
+btn.addEventListener("click", () =>{
+
+   if(i.getAttribute("name") === "moon-sharp") {
+       console.log("moon detected");
+       i.classList.toggle(i.setAttribute("name", "sunny-sharp"));
+   }
+   else
+   {
+    i.classList.toggle(i.setAttribute("name", "moon-sharp"));
+   }
+ 
+
+   container.classList.toggle("dark-mode-color");
+  
 })
