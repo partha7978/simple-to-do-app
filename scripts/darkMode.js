5396f0f515ebae7e6aba07298949c110c5fe0421
partha7978 <muduliparthasarathi@gmail.com>
2021-09-23 09:21:55 +0530

    list dark mode half done

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -9,41 +9,60 @@ let inputBtn = document.querySelector(".inputAddButton");
 
 let unorderedList = document.querySelector("ul");
 
+btn.addEventListener("click", () => {
+  darkModeFunction();
+  listDarkMode();
+});
 
-btn.addEventListener("click", () =>{
-
-   if(i.getAttribute("name") === "moon-sharp") {
-       i.classList.toggle(i.setAttribute("name", "sunny-sharp"));
-   }
-   else
-   {
+const darkModeFunction = () => {
+  if (i.getAttribute("name") === "moon-sharp") {
+    i.classList.toggle(i.setAttribute("name", "sunny-sharp"));
+  } else {
     i.classList.toggle(i.setAttribute("name", "moon-sharp"));
-   }
+  }
 
-   //for adding dark mode in lists starts
-    //    console.log(unorderedList.childNodes.length);
-   
-   if(unorderedList.childNodes.length-3 > 0) {
-       console.log("working");
-       let lists = document.querySelectorAll("li");
-        // console.log(lists[1]);
-        // console.log(lists);
-        // console.log(lists.length, "list length");
-        for(let i=0; i<= lists.length-1; i++) {
-            lists[i].classList.toggle("li-hover-shadow");
-            lists[i].classList.toggle("li-dark-color");
-        }
-    
-   }
-   //for adding dark mode in lists ends
+  container.classList.toggle("dark-mode-color");
+  header.classList.toggle("dark-mode-color");
+  btn.classList.toggle("dark-mode-color-for-btn");
+  mainContent.classList.toggle("main-content-dark-mode");
+  inputTime.classList.toggle("input-dark-mode");
+  input.classList.toggle("input-dark-mode");
+  inputBtn.classList.toggle("btn-dark-color");
+};
 
-   container.classList.toggle("dark-mode-color");
-   header.classList.toggle("dark-mode-color");
-   btn.classList.toggle("dark-mode-color-for-btn");
-   mainContent.classList.toggle("main-content-dark-mode");
-   inputTime.classList.toggle("input-dark-mode");
-   input.classList.toggle("input-dark-mode");
-   inputBtn.classList.toggle("btn-dark-color");
-   
-//    lists.classList.toggle("li-hover-shadow");
-})
+const listDarkMode = () => {
+  //for adding dark mode in lists starts
+  //    console.log(unorderedList.childNodes.length);
+
+  if (unorderedList.childNodes.length - 3 > 0) {
+    console.log("working");
+    let lists = document.querySelectorAll("li");
+    // console.log(lists[1]);
+    // console.log(lists);
+    // console.log(lists.length, "list length");
+    for (let i = 0; i <= lists.length - 1; i++) {
+      lists[i].classList.toggle("li-hover-shadow");
+      lists[i].classList.toggle("li-dark-color");
+
+
+      if(document.querySelector(".li-hover-shadow")) {
+console.log("li detected");
+      }
+    }
+  }
+  //for adding dark mode in lists ends
+};
+
+
+
+
+
+
+// for update the color during adding of lists starts
+if (document.querySelector(".dark-mode-color")) {
+    listDarkMode();
+  console.log("dark mode detected");
+} else {
+  console.log("light mode detected");
+}
+// for update the color during adding of lists ends
