34e720c8972696a9949fdaf09396f878f3756148
partha7978 <muduliparthasarathi@gmail.com>
2022-04-07 20:04:24 +0530

    adding files for custom popup box

---
+++ scripts/popup.js
@@ -0,0 +1,4 @@
+let model = document.getElementById("popup");
+let modelContent = model.querySelector(".popup-content");
+let span = document.querySelector(".close");
+let btn = document.querySelector(".closeBtn");
