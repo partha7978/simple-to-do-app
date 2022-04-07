ac0284810c67efe5ffab10dcf7ee22d557d1c1f9
partha7978 <muduliparthasarathi@gmail.com>
2022-04-07 20:36:18 +0530

    adding popup when dark mode detected

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,48 @@
-let model = document.getElementById("popup");
+let model = document.getElementById("myPopup1");
 let modelContent = model.querySelector(".popup-content");
-let span = document.querySelector(".close");
-let btn = document.querySelector(".closeBtn");
+let closeBtn = document.querySelector(".closeBtn");
+// When the user clicks the button, open the model
+
+const forDarkMode = () => {
+  setTimeout(() => {
+    model.style.display = "block";
+    modelContent.classList.add("animate__animated", "animate__fadeInDown"); //!   adding fade in animation when it appears
+  }, 2000);
+
+  closeBtn.addEventListener("click", () => {
+    modelContent.classList.add("animate__animated", "animate__fadeOutDown");
+    setTimeout(() => {
+      model.style.display = "none";
+    }, 600);
+  });
+
+  // When the user clicks anywhere outside of the model, close it
+  window.onclick = function (event) {
+    if (event.target == model) {
+      modelContent.classList.add("animate__animated", "animate__fadeOutDown");
+      setTimeout(() => {
+        model.style.display = "none";
+      }, 600);
+    }
+  };
+};
+
+//! for system theme detection and set the theme according to that STARTS
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  // window.location.reload(); //for reloding
+  darkModeFunction();
+
+  console.log("dark mode detected"); // dark mode
+  forDarkMode();
+} else {
+  console.log("light mode detected"); // light mode
+  setTimeout(() => {
+    alert(
+      "The webpage is in light mode, You can also try the dark mode by clicking on the top right corner icon."
+    );
+  }, 3000);
+}
+//for system theme detection and set the theme according to that ENDS
