3f1ea82b6ff7716e95ac56f01bcb15f01afe5e02
partha7978 <muduliparthasarathi@gmail.com>
2022-04-08 00:30:52 +0530

    adding light mode popup message

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,11 @@
 let model = document.getElementById("myPopup1");
 let modelContent = model.querySelector(".popup-content");
 let closeBtn = document.querySelector(".closeBtn");
+
+let model2 = document.getElementById("myPopup2");
+let modelContent2 = model2.querySelector(".popup-content2");
+let closeBtn2 = document.querySelector(".close-btn2")
+
 // When the user clicks the button, open the model
 
 const forDarkMode = () => {
@@ -17,8 +22,8 @@ const forDarkMode = () => {
   });
 
   // When the user clicks anywhere outside of the model, close it
-  window.onclick = function (event) {
-    if (event.target == model) {
+  window.onclick = function (e) {
+    if (e.target == model) {
       modelContent.classList.add("animate__animated", "animate__fadeOutDown");
       setTimeout(() => {
         model.style.display = "none";
@@ -26,6 +31,32 @@ const forDarkMode = () => {
     }
   };
 };
+
+
+const forLightMode = () => {
+  setTimeout(() => {
+    model2.style.display = "block";
+    modelContent2.classList.add("animate__animated", "animate__fadeInDown"); //!   adding fade in animation when it appears
+  }, 2000);
+
+
+  closeBtn2.addEventListener("click", () => {
+    modelContent2.classList.add("animate__animated", "animate__fadeOutDown");
+    setTimeout(() => {
+      model2.style.display = "none";
+    }, 600);
+  });
+
+  // When the user clicks anywhere outside of the model, close it
+  window.onclick = function (e) {
+    if (e.target == model2) {
+      modelContent2.classList.add("animate__animated", "animate__fadeOutDown");
+      setTimeout(() => {
+        model2.style.display = "none";
+      }, 600);
+    }
+  };
+}
 
 //! for system theme detection and set the theme according to that STARTS
 if (
@@ -37,12 +68,10 @@ if (
 
   console.log("dark mode detected"); // dark mode
   forDarkMode();
-} else {
+}
+else
+{
   console.log("light mode detected"); // light mode
-  setTimeout(() => {
-    alert(
-      "The webpage is in light mode, You can also try the dark mode by clicking on the top right corner icon."
-    );
-  }, 3000);
+  forLightMode();
 }
 //for system theme detection and set the theme according to that ENDS
